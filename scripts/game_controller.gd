0a5b212d41a6b97fb43ab14849d5b1f9746fe112
LucasJappert <lucasnicolasjappert@gmail.com>
2025-03-08 10:18:24 -0300

    names improvements

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,37 +1,37 @@
 extends Node2D
 
 # Ruta de la escena base del monstruo
-const MONSTER_BASE_SCENE = preload("res://scenes/monster/MonsterBase.tscn")
+const ENEMY_SCENE = preload("res://scenes/monster/enemy.tscn")
 const PLAYER_SCENE = preload("res://scenes/player/player.tscn")
 
 # Carga los recursos SpriteFrames directamente en el código
-const MONSTER1_SPRITE_FRAMES = preload("res://resources/sprite-frames/monster1.tres")
-const MONSTER2_SPRITE_FRAMES = preload("res://resources/sprite-frames/monster2.tres")
+const ENEMY1_SPRITE_FRAMES = preload("res://resources/sprite-frames/monster1.tres")
+const ENEMY2_SPRITE_FRAMES = preload("res://resources/sprite-frames/monster2.tres")
 
 const PLAYER_SPRITES = preload("res://resources/sprite-frames/player.tres")
 
 func _ready():
-	print("Agregamos el player")
+	print("Add the player")
 	_spawn_player()
 
-	print("Agregamos monsters")
+	print("Add some enemies")
 	for i in range(35):  # Genera 5 monstruos
 		# Elige un tipo de monstruo al azar
-		var monster_type = randi() % 2
-		if monster_type == 0:
-			_spawn_monster(MONSTER1_SPRITE_FRAMES, i)
+		var enemy_type = randi() % 2
+		if enemy_type == 0:
+			_spawn_enemy(ENEMY1_SPRITE_FRAMES, i)
 		else:
-			_spawn_monster(MONSTER2_SPRITE_FRAMES, i)
+			_spawn_enemy(ENEMY2_SPRITE_FRAMES, i)
 
-func _spawn_monster(sprite_frames: SpriteFrames, id: int = 0):
-	var monster = MONSTER_BASE_SCENE.instantiate()
+func _spawn_enemy(sprite_frames: SpriteFrames, id: int = 0):
+	var new_enemy = ENEMY_SCENE.instantiate()
 	
 	var screen = get_viewport_rect().size
-	monster.position = Vector2(randf() * screen.x, randf() * screen.y)
-	monster.ID = str(id)
-	monster.sprite_frames = sprite_frames
+	new_enemy.position = Vector2(randf() * screen.x, randf() * screen.y)
+	new_enemy.ID = str(id)
+	new_enemy.sprite_frames = sprite_frames
 
-	add_child(monster)
+	add_child(new_enemy)
 
 func _spawn_player():
 	var player = PLAYER_SCENE.instantiate()
